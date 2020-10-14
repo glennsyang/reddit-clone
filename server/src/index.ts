8a5349f11b3087c081c4e6f6e8a4f7d19f75246f
Glenn Sheppard <gsheppard.yang@gmail.com>
2020-10-14 14:05:49 -0700

    update package.json and sameSite=none

--- server/src/index.ts
+++ server/src/index.ts
@@ -53,7 +53,7 @@ const main = async () => {
             cookie: {
                 maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
                 httpOnly: true,
-                sameSite: "lax", // csrf
+                sameSite: __prod__ ? "none" : "lax", // csrf
                 secure: __prod__, // cookie only works in https
                 domain: __prod__ ? ".glennsheppard.dev" : undefined,
             },
